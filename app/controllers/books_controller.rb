b3d2292232770df41cfca85bba5cb52e1ede1193
Patrick Sarson <sarsonmedia@gmail.com>
2017-03-15 15:15:01 -0400

    add view dir update book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,28 @@ class BooksController < BlocWorks::Controller
   def welcome
     render :welcome, book: "Eloquent Ruby"
   end
+
+  def new
+    #directs to create#new page
+  end
+
+  def create
+    #creates book object
+  end
+
+  def edit
+    #directs to editing page
+  end
+
+  def update
+    # access the book array
+    # renders old value
+    # take input
+    # renders new value
+  end
+
+  def destroy
+    # removes item from array
+  end
+
 end
