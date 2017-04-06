3f502ac966489eea8417ae6d69e1abaa8c5f3b95
Patrick Sarson <sarsonmedia@gmail.com>
2017-04-06 18:27:43 -0400

    Add the rest of checkpoint-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < BlocWorks::Controller
   def welcome
     render :welcome, book: "Eloquent Ruby"
   end
+
+  def index
+     render :index, books: Book.all
+   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require "bloc_works"
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "models")
+
+ BlocRecord.connect_to("db/db.sqlite")
+
 
 module BlocBooks
   class Application < BlocWorks::Application
